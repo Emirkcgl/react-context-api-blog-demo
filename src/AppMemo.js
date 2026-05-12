45d4a848e23ff301eb77f20138cdc04487c4baab
Emirkcgl <yunus0598koca@gmail.com>
2026-05-12 17:45:53 +0300

    refactor:useMemo

--- src/AppMemo.js
+++ src/AppMemo.js
@@ -1,4 +1,4 @@
-import { useEffect, useState, memo } from "react";
+import { useEffect, useState, memo, useMemo } from "react";
 import { faker } from "@faker-js/faker";
 
 function createRandomPost() {
@@ -9,7 +9,6 @@ function createRandomPost() {
 }
 
 function AppMemo() {
-    const [show, setShow] = useState(false);
     const [posts, setPosts] = useState(() =>
         Array.from({ length: 30 }, () => createRandomPost())
     );
@@ -42,6 +41,14 @@ function AppMemo() {
         [isFakeDark]
     );
 
+
+    const archiveOptions = useMemo(() => {
+        return {
+            show: false,
+            title: `Post archive in addtion ${posts.length} posts`,
+        };
+    }, [posts.length]);
+
     return (
         <section>
             <button
@@ -58,7 +65,7 @@ function AppMemo() {
                 setSearchQuery={setSearchQuery}
             />
             <Main posts={searchedPosts} onAddPost={handleAddPost} />
-            <Archive show={show} />
+            <Archive archiveOptions={archiveOptions} />
             <Footer />
         </section>
     );
@@ -155,14 +162,14 @@ function List({ posts }) {
     );
 }
 
-const Archive = memo(function Archive({ show }) {
+const Archive = memo(function Archive({ archiveOptions }) {
     // Here we don't need the setter function. We're only using state to store these posts because the callback function passed into useState (which generates the posts) is only called once, on the initial render. So we use this trick as an optimization technique, because if we just used a regular variable, these posts would be re-created on every render. We could also move the posts outside the components, but I wanted to show you this trick 😉
     const [posts] = useState(() =>
         // 💥 WARNING: This might make your computer slow! Try a smaller `length` first
         Array.from({ length: 30000 }, () => createRandomPost())
     );
 
-    const [showArchive, setShowArchive] = useState(show);
+    const [showArchive, setShowArchive] = useState(archiveOptions.show);
 
     return (
         <aside>
